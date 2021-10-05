c31af4d4d7aa9c756f0896d244807b2b12a37621
Igor <igor_csns_s2@hotmail.com>
2021-10-05 13:47:10 -0300

    feat(api): api documentation

--- src/app/todo/dto/create-todo.dto.ts
+++ src/app/todo/dto/create-todo.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsIn, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateTodoDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   task: string;
 
   @IsNotEmpty()
   @IsIn([0, 1])
+  @ApiProperty()
   isDone: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('TODOApp API')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
